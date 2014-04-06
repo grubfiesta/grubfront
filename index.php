deecd26a5c4ed4f60ce812af475221c1ea26a0e4
Tirth Bodawala <tirthbodawala@atyantik.com>
2014-04-06 11:39:13 +0530

    Item request is now working

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
     <!-- Custom styles for this template -->
     <link href="/assets/css/bootstrap-theme.css" rel="stylesheet">
     <link href="/assets/css/style.css" rel="stylesheet">
+    <script type="text/javascript">
+        <?php
+            $env =  getenv('APPLICATION_ENV');
+            if(!empty($env)){
+                $env = 'local';
+            }
+        ?>
+        Base.ENV = typeof window["ENV"] != "undefined" ? <?php echo $env ?>;
+    </script>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
